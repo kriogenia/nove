e3cffe86c8cd4457548aea81e5ef1a556debaede
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-03-04 23:52:19 +0000

    Fix compare

--- tracing/src/main.rs
+++ tracing/src/main.rs
@@ -15,18 +15,12 @@ fn main() {
     let content = std::fs::read(args.file).expect("failed to read rom file");
     let rom = Rom::new(&content).unwrap();
 
-    let mut count = 0;
-
     let mut core = NesNoveCore::new(rom);
     core.reset();
     core.pc = 0xC000;
 
     println!("{}", trace::trace(&core));
     core.run(move |core| {
-        count += 1;
-        if count > 100 {
-            std::process::exit(0);
-        }
         println!("{}", trace::trace(core));
     })
     .unwrap();
