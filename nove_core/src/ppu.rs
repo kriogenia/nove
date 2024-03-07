731d74aceb551023695360282956ccb2a379565c
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-03-07 19:46:15 +0000

    Add VRAM read to the PPU

--- nove_core/src/ppu.rs
+++ nove_core/src/ppu.rs
@@ -9,8 +9,15 @@ mod control_register;
 const PALETTE_SIZE: usize = 32;
 const VRAM_SIZE: usize = 2048; // 2 KiB
 const OAM_SIZE: usize = 256;
+const NAMETABLE_SIZE: u16 = 1024; // 1KiB
 
 const LIMIT_ADDR: u16 = 0x3fff;
+
+const CHROM_START_ADDR: u16 = 0x0000;
+const VRAM_START_ADDR: u16 = 0x2000;
+const PALETTE_START_ADDR: u16 = 0x3f00;
+const CHROM_END_ADDR: u16 = VRAM_START_ADDR - 1;
+const VRAM_END_ADDR: u16 = 0x2fff;
 
 pub struct PPU {
     chrom: Program,
@@ -20,7 +27,7 @@ pub struct PPU {
     vram: [u8; VRAM_SIZE],
     oam: [u8; OAM_SIZE],
     mirroring: Mirroring,
-    internal_data_bufffer: u8,
+    internal_data_buffer: u8,
 }
 
 impl PPU {
@@ -33,7 +40,7 @@ impl PPU {
             vram: [Default::default(); VRAM_SIZE],
             oam: [Default::default(); OAM_SIZE],
             mirroring,
-            internal_data_bufffer: Default::default(),
+            internal_data_buffer: Default::default(),
         }
     }
 
@@ -41,17 +48,18 @@ impl PPU {
         self.ctrl.vram_add_inc();
         let addr = self.addr.get();
         match addr {
-            0x0000..=0x1fff => self.read_and_store(self.chrom[addr as usize]),
-            0x2000..=0x2fff => todo!("read from ram"),
-            0x3000..=0x3eff => panic!("invalid access to address {}", self.addr.get()),
-            0x3f00..=LIMIT_ADDR => todo!("read from palette"),
+            CHROM_START_ADDR..=CHROM_END_ADDR => self.read_and_store(self.chrom[addr as usize]),
+            VRAM_START_ADDR..=VRAM_END_ADDR => {
+                self.read_and_store(self.vram[self.mirror_vram_addr(addr) as usize])
+            }
+            PALETTE_START_ADDR..=LIMIT_ADDR => todo!("read from palette"),
             _ => panic!("invalid access to mirrored space: {}", self.addr.get()),
         }
     }
 
     fn read_and_store(&mut self, val: u8) -> u8 {
-        let prev = self.internal_data_bufffer;
-        self.internal_data_bufffer = val;
+        let prev = self.internal_data_buffer;
+        self.internal_data_buffer = val;
         prev
     }
 
@@ -61,6 +69,18 @@ impl PPU {
 
     fn write_ctrl(&mut self, val: u8) {
         self.ctrl.set(val)
+    }
+
+    fn mirror_vram_addr(&self, addr: u16) -> u16 {
+        let vram_addr = (addr & VRAM_END_ADDR) - VRAM_START_ADDR;
+        let name_table = vram_addr / NAMETABLE_SIZE;
+        vram_addr
+            - match (&self.mirroring, name_table) {
+                (Mirroring::Vertical, 2) | (Mirroring::Vertical, 3) => 2 * NAMETABLE_SIZE,
+                (Mirroring::Horizontal, 1) | (Mirroring::Horizontal, 2) => NAMETABLE_SIZE,
+                (Mirroring::Horizontal, 3) => 2 * NAMETABLE_SIZE,
+                _ => 0,
+            }
     }
 }
 
@@ -72,9 +92,41 @@ mod test {
     #[test]
     fn read_chrom() {
         let mut ppu = PPU::new(vec![0, 1, 2, 3], Mirroring::Horizontal);
-        ppu.write_addr(0x00);
-        ppu.write_addr(0x01);
-        assert_ne!(1, ppu.read_data());
-        assert_eq!(1, ppu.read_data());
+        assert_read(&mut ppu, 0x00, 0x01, 1);
+    }
+
+    #[test]
+    fn read_vram_horizontal() {
+        let mut ppu = PPU::new(vec![], Mirroring::Horizontal);
+        ppu.vram[0x0002] = 3;
+        ppu.vram[0x0020] = 4;
+        ppu.vram[0x0402] = 5;
+        ppu.vram[0x0420] = 6;
+
+        assert_read(&mut ppu, 0x20, 0x02, 3);
+        assert_read(&mut ppu, 0x24, 0x20, 4);
+        assert_read(&mut ppu, 0x28, 0x02, 5);
+        assert_read(&mut ppu, 0x2c, 0x20, 6);
+    }
+
+    #[test]
+    fn read_vram_vertical() {
+        let mut ppu = PPU::new(vec![], Mirroring::Vertical);
+        ppu.vram[0x0002] = 3;
+        ppu.vram[0x0020] = 4;
+        ppu.vram[0x0402] = 5;
+        ppu.vram[0x0420] = 6;
+
+        assert_read(&mut ppu, 0x20, 0x02, 3);
+        assert_read(&mut ppu, 0x24, 0x02, 5);
+        assert_read(&mut ppu, 0x28, 0x20, 4);
+        assert_read(&mut ppu, 0x2c, 0x20, 6);
+    }
+
+    fn assert_read(ppu: &mut PPU, hi: u8, lo: u8, val: u8) {
+        ppu.write_addr(hi);
+        ppu.write_addr(lo);
+        assert_ne!(val, ppu.read_data());
+        assert_eq!(val, ppu.read_data());
     }
 }
